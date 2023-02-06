c589f19d834ac2456b41cdba38f77672124cac07
prosper <anosikeprosper2@gmail.com>
2023-02-06 12:01:51 -0800

    refactored the authRouter file

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const authRouter = express.Router();
 
-const { validate, validateUser } = require("../middleware/validator");
+// const { validate, validateUser } = require("../middleware/validator");
 const { userSignup, userLogin } = require("../controllers/authController");
 
 // authRouter.post("/signup", validateUser(), validate, userSignup);
@@ -11,4 +11,6 @@ authRouter.post("/signup", userSignup);
 
 authRouter.post("/login", userLogin);
 
-module.exports = { authRouter };
+// authRouter.post("/login", userLogin);
+
+module.exports = authRouter;
