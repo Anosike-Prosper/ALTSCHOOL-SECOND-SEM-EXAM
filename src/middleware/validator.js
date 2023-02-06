db5ca041309558af2b4d2c51b25c916392ef8122
prosper <anosikeprosper2@gmail.com>
2023-02-06 12:04:28 -0800

    commented out the validator code

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,29 +1,29 @@
-const { body, validationResult } = require("express-validator");
+// const { body, validationResult } = require("express-validator");
 
-// THIS MIDDLEWARE RUNS A VALIDATION ON THE USER INPUT WHEN A SIGN UP OCCURS
-function validateUser() {
-  return [
-    body("firstname").notEmpty().isString(),
-    body("lastname").notEmpty().isString(),
-    body("email").notEmpty().isEmail(),
-    body("password").notEmpty().isString(),
-  ];
-}
+// // THIS MIDDLEWARE RUNS A VALIDATION ON THE USER INPUT WHEN A SIGN UP OCCURS
+// function validateUser() {
+//   return [
+//     body("firstname").notEmpty().isString(),
+//     body("lastname").notEmpty().isString(),
+//     body("email").notEmpty().isEmail(),
+//     body("password").notEmpty().isString(),
+//   ];
+// }
 
-function validate(req, res, next) {
-  const errors = validationResult(req);
+// function validate(req, res, next) {
+//   const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    return next();
-  }
+//   if (errors.isEmpty()) {
+//     return next();
+//   }
 
-  const extractedErrors = [];
+//   const extractedErrors = [];
 
-  errors.array().map((err) => {
-    extractedErrors.push({ [err.param]: err.msg });
-  });
+//   errors.array().map((err) => {
+//     extractedErrors.push({ [err.param]: err.msg });
+//   });
 
-  return res.status(401).json({ message: extractedErrors });
-}
+//   return res.status(401).json({ message: extractedErrors });
+// }
 
-module.exports = { validateUser, validate };
+// module.exports = { validateUser, validate };
