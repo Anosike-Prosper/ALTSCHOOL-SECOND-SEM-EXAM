abd94ef290490adb64624c33827c774eb9d0d5f6
prosper <anosikeprosper2@gmail.com>
2023-02-06 12:15:40 -0800

    commented the code in this file

--- test/test.test.js
+++ test/test.test.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-const posts = require("./post.test.js");
-const login = require("./user_login.test.js");
-const signup = require("./user_signup.test.js");
-const app = require("../src/app");
-const supertest = require("supertest");
-const api = supertest(app);
+// const mongoose = require("mongoose");
+// const posts = require("./post.test.js");
+// const login = require("./user_login.test.js");
+// const signup = require("./user_signup.test.js");
+// const app = require("../src/app");
+// const supertest = require("supertest");
+// const api = supertest(app);
 
-describe("tests", () => {
-  login(api);
-  signup(api);
-  posts(api);
-});
+// describe("tests", () => {
+//   login(api);
+//   signup(api);
+//   posts(api);
+// });
 
-afterAll(() => {
-  mongoose.connection.close();
-});
+// afterAll(() => {
+//   mongoose.connection.close();
+// });
