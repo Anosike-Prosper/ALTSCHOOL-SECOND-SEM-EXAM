4e76646b64d4ee29dbeaec72c41764f9dd824039
prosper <anosikeprosper2@gmail.com>
2023-02-06 12:17:38 -0800

    commented the code in this file

--- test/user_signup.test.js
+++ test/user_signup.test.js
@@ -1,52 +1,58 @@
 const userModel = require("../src/models/userModel");
+const request = require("supertest");
+const app = require("../././src/app");
 
-module.exports = (api) =>
-  describe("", () => {
-    beforeAll(async () => {
-      await userModel.deleteMany({});
-    });
+// module.exports = (api) =>
+//   describe("", () => {
 
-    describe("post request to api/signup", () => {
-      test("with correct details successfully creates a user", async () => {
-        const newUser = {
-          firstname: "james",
-          lastname: "mark",
-          email: "jamegs@mail.com",
-          password: "password",
-        };
+//     beforeAll(async () => {
 
-        const usersInDbBefore = await userModel.countDocuments({});
-        const response = await api
-          .post("/user/signup")
-          .send(newUser)
-          .expect(201)
-          .expect("Content-Type", /application\/json/);
+//       await userModel.deleteMany({});
 
-        const usersInDbAfter = await userModel.countDocuments({});
-        expect(usersInDbBefore).toBe(usersInDbAfter - 1);
+//     });
 
-        expect(response.body).toHaveProperty("message");
-        expect(response.body.data).not.toContain("password");
-        expect(response.body.data.firstName).toEqual(newUser.firstName);
-      });
+//     describe("post request to api/signup", () => {
+//       test("with correct details successfully creates a user", async () => {
+//         const newUser = {
+//           firstname: "james",
+//           lastname: "mark",
+//           email: "jamegs@mail.com",
+//           password: "password",
+//         };
 
-      test("with incorrect details returns an error", async () => {
-        const newUser = {
-          firstname: "james",
-          lastname: "mark",
-          email: "jamegs@mail.com",
-          password: "password",
-        };
+//         const usersInDbBefore = await userModel.countDocuments({});
+//         console.log('---------',usersInDbBefore)
+//         const response = await api
+//           .post("/user/signup")
+//           .send(newUser)
+//           .expect(201)
+//           .expect("Content-Type", /application\/json/);
 
-        const usersInDbBefore = await userModel.count({});
-        await api
-          .post("/user/signup")
-          .send(newUser)
-          .expect(400)
-          .expect("Content-Type", /application\/json/);
+//         // const usersInDbAfter = await userModel.countDocuments({});
+//         // expect(usersInDbBefore).toBe(usersInDbAfter - 1);
 
-        const usersInDbAfter = await userModel.count({});
-        expect(usersInDbBefore.length).toBe(usersInDbAfter.length);
-      });
-    });
-  });
+//         // expect(response.body).toHaveProperty("message");
+//         // expect(response.body.data).not.toContain("password");
+//         // expect(response.body.data.firstName).toEqual(newUser.firstName);
+//       });
+
+//       test("with incorrect details returns an error", async () => {
+//         const newUser = {
+//           firstname: "james",
+//           lastname: "mark",
+//           email: "jamegs@mail.com",
+//           password: "password",
+//         };
+
+//         const usersInDbBefore = await userModel.count({});
+//         await api
+//           .post("/user/signup")
+//           .send(newUser)
+//           .expect(400)
+//           .expect("Content-Type", /application\/json/);
+
+//         const usersInDbAfter = await userModel.count({});
+//         expect(usersInDbBefore.length).toBe(usersInDbAfter.length);
+//       });
+//     });
+// });
