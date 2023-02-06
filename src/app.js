e30f29ab1ca674c8fa7329c91f0d5f76224608a1
prosper <anosikeprosper2@gmail.com>
2023-02-06 11:49:28 -0800

    refactored the app.js file

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
-const app = express();
 
 require("dotenv").config();
+const app = express();
 
+const { connectToMongoDB } = require("./database/db");
 const bodyParser = require("body-parser");
 
 const AppError = require("../utils/appError");
 
 const { postRouter } = require("./routers/postRouter");
 
-const { authRouter } = require("./routers/authRouter");
-const { connectToMongoDB } = require("./database/db");
-const { globalError } = require("./controllers/errorController");
+const authRouter = require("./routers/authRouter");
 
-const PORT = process.env.PORT;
+const { globalError } = require("./controllers/errorController");
 
 app.use(express.json());
 
@@ -30,21 +29,15 @@ app.use("/user", authRouter);
 
 app.use("/post", postRouter);
 
-connectToMongoDB();
-
-app.listen(PORT, () => {
-  console.log(`App is listening on localhost:${PORT}`);
-});
-
 app.all("*", (req, res, next) => {
   next(new AppError(`Cannot find ${req.originalUrl} in the server`, 404));
 });
 
 app.use(globalError);
-const handleValidationErrorDB = (err) => {
-  const errors = Object.values(err.errors).map((el) => el.message);
-  const message = `Invalid input data. ${errors.join(". ")}`;
-  return new AppError(message, 400);
-};
+connectToMongoDB();
 
 module.exports = app;
+
+// const app = require("./app");
+
+// const PORT = process.env.PORT;
