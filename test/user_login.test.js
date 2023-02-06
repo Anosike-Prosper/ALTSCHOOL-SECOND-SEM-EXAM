2ef4c9b59beca8472b03abf108e3a5965327e2f3
prosper <anosikeprosper2@gmail.com>
2023-02-06 12:16:52 -0800

    commented the code in this file

--- test/user_login.test.js
+++ test/user_login.test.js
@@ -1,58 +1,58 @@
-const userModel = require("../src/models/userModel");
+// const userModel = require("../src/models/userModel");
 
-module.exports = (api) =>
-  describe("", () => {
-    beforeAll(async () => {
-      await userModel.deleteMany({});
-      await userModel.create({
-        firstname: "james",
-        lastname: "mark",
-        email: "jamegs@mail.com",
-        password: "password",
-      });
-    });
+// module.exports = (api) =>
+//   describe("", () => {
+//     beforeAll(async () => {
+//       await userModel.deleteMany({});
+//       await userModel.create({
+//         firstname: "james",
+//         lastname: "mark",
+//         email: "jamegs@mail.com",
+//         password: "password",
+//       });
+//     });
 
-    describe("POST request to /user/login", () => {
-      test("is successful if user is registered in the database", async () => {
-        const response = await api
-          .post("/user/login")
-          .send({
-            email: "jamegs@mail.com",
-            password: "password",
-          })
-          .expect(200);
+//     describe("POST request to /user/login", () => {
+//       test("is successful if user is registered in the database", async () => {
+//         const response = await api
+//           .post("/user/login")
+//           .send({
+//             email: "jamegs@mail.com",
+//             password: "password",
+//           })
+//           .expect(200);
 
-        expect(response.body).toHaveProperty("token");
-      });
+//         expect(response.body).toHaveProperty("token");
+//       });
 
-      test("returns error if incorrect details are sent", async () => {
-        const response = await api
-          .post("/user/login")
-          .send({
-            email: "kate@gmail.com",
-            password: "2133344",
-          })
-          .expect(404);
+//       test("returns error if incorrect details are sent", async () => {
+//         const response = await api
+//           .post("/user/login")
+//           .send({
+//             email: "kate@gmail.com",
+//             password: "2133344",
+//           })
+//           .expect(404);
 
-        expect(response.body).not.toHaveProperty("token");
-      });
+//         expect(response.body).not.toHaveProperty("token");
+//       });
 
-      test("returns error if  an empty email field  is sent", async () => {
-        const response = await api
-          .post("/user/login")
-          .send({
-            email: "",
-          })
-          .expect(400);
-      });
+//       test("returns error if  an empty email field  is sent", async () => {
+//         const response = await api
+//           .post("/user/login")
+//           .send({
+//             email: "",
+//           })
+//           .expect(400);
+//       });
 
-      test("returns error if an empty password field  is sent", async () => {
-        const response = await api
-          .post("/user/login")
-          .send({
-            password: "",
-          })
-          .expect(400);
-      });
-    });
-  });
+//       test("returns error if an empty password field  is sent", async () => {
+//         const response = await api
+//           .post("/user/login")
+//           .send({
+//             password: "",
+//           })
+//           .expect(400);
+//       });
+//     });
+//   });
